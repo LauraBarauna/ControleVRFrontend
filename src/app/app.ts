c17fd725fc3b34eb4b08cc473992aa2432217328
Laura Barauna <laura.barauna@senior.com.br>
2026-04-02 11:00:44 -0300

    Adiciona primeNg e configura.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
 }
