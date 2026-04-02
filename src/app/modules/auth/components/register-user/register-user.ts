f0f3c121b533b32bfc0a81a5544ae86b35f1b2ad
Laura Barauna <laura.barauna@senior.com.br>
2026-04-02 11:49:48 -0300

    [FEAT][VR-002]: Cria tela de cadastro.

--- src/app/modules/auth/components/register-user/register-user.ts
+++ src/app/modules/auth/components/register-user/register-user.ts
@@ -1,9 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.html',
   styleUrl: './register-user.css',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    FloatLabelModule
+  ],
 })
-export class RegisterUser {
+export class RegisterUser implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required, Validators.minLength(3), Validators.maxLength(50)],
+      lastName: ['', Validators.required, Validators.minLength(2), Validators.maxLength(100)],
+      username: ['', Validators.required, Validators.minLength(4), Validators.maxLength(80)],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+
+    }
+  }
 }
