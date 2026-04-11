b7c28ea95e7499ce680cb35f02c288bf84eac8e0
Laura Barauna <lauravbarauna@gmail.com>
2026-04-11 18:36:32 -0300

    [FEAT][VR-005]: Adiciona HTML para visualização.

--- src/app/modules/security/profile/components/read-profile/read-profile.ts
+++ src/app/modules/security/profile/components/read-profile/read-profile.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { TagModule } from 'primeng/tag';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { CardModule } from 'primeng/card';
+
+export const roles = {
+  ROLE_USER: 'Usuário',
+  ADMIN_ROLE: 'Administrador',
+} as const;
+
 
 @Component({
   selector: 'app-read-profile',
-  imports: [],
+  imports: [TagModule, DividerModule, ButtonModule, AvatarModule, CardModule],
   templateUrl: './read-profile.html',
   styleUrl: './read-profile.css',
 })
-export class ReadProfile {}
+
+export class ReadProfile {
+  user = {
+    id: 1,
+    firstName: 'Laura',
+    lastName: 'Isabela',
+    username: 'laura.isabela',
+    role: roles['ADMIN_ROLE'],
+  };
+  protected readonly roles = roles;
+}
